28907df8bafdc1a4d4ff040876fa7e039f4a59e0
Alexandre Morand <alexmorand26@gmail.com>
2015-10-23 15:15:57 +0200

    cleaning

--- app/models/Morpion/game.rb
+++ app/models/Morpion/game.rb
@@ -14,27 +14,32 @@ module Morpion
     end
 
 		def to_s
-		      case player
-		      when :user
-		        "X"
-		      when :computer
-		        "O"
-		      when :none
-		        "."
-		      end     
+      case player
+      when :user
+        "X"
+      when :computer
+        "O"
+      when :none
+        "."
+      end     
 		end   
 	end
 
+
 	class Alignment
-    attr_accessor :boxes
+    	attr_accessor :boxes
     def initialize
       self.boxes = []
+    end
+    def is_won?
+    	v = boxes.map(&:player).uniq
+    	v.count == 1 && v.first != :none
     end
   end
 
 
 	class Board 
-		attr_accessor :boxes
+			attr_accessor :boxes
     	attr_accessor :alignments
 
 		def initialize
@@ -97,178 +102,55 @@ module Morpion
 			str
 		end
 
-
-		#On parcours toutes les solution possible par alignement
-		def tmp_show_all_al
-			alignments.each do |alignment|
-				alignment.boxes.each do |box| 
-					box.player = :computer 
-				end
-				puts self
-				alignment.boxes.each do |box| 
-					box.player = :none 
-				end
-			end
+		def is_game_over
+			result = alignments.select{|a| a.is_won?}
+			result.count > 0
 		end
-
-		# def checkrow (row)
-		# 	response =""
-		# 		(0..9).each do |j|
-		# 			response += "#{self.boxes[row][j]}"
-		# 		end
-		# 	if 	response.include?("XXXXX") 
-		# 		puts "Le joueur 1 à gagné par ligne"
-		# 		@fin = true
-		# 	elsif response.include?("OOOOO")
-		# 		puts "Le joueur 2 à gagné par ligne"
-		# 		@fin = true
-		# 	end
-		# end
-
-		# def checkcol (col)
-		# 	response = ""
-		# 		(0..9).each do |i|
-		# 			response += "#{self.boxes[i][col]}"
-		# 		end
-		# 	if 	response.include?("XXXXX") 
-		# 		puts "Le joueur 1 à gagné par colonne"
-		# 		@fin = true
-		# 	elsif response.include?("OOOOO")
-		# 		puts "Le joueur 2 à gagné par colonne"
-		# 		@fin = true
-		# 	end
-		# end
-
-		# def checkdiag_sup_right(diag, j)
-		# 	response = ""
-		# 	computerwin = "XXXXX"
-		# 	userwin = "OOOOO"
-		# 	if j == 0
-		# 		(diag-4..9).each do |i|				
-		# 			response += "#{self.boxes[i][i-diag+4]}"
-		# 		end
-		# 	elsif j == 1
-		# 		(0..9).each do |i|	
-		# 			response += "#{self.boxes[i][9+i-diag]}"
-		# 		end
-		# 	end
-		# 	if 	response.include?("XXXXX") 
-		# 		puts "Le joueur 1 à gagné par diag"
-		# 		@fin = true
-		# 	elsif response.include?("OOOOO")
-		# 		puts "Le joueur 2 à gagné par diag"
-		# 		@fin = true
-		# 	end
-		# end
-		
-
-		# def checkdiag_sup_left(diag, j)
-		# 	response = ""
-		# 	computerwin = "XXXXX"
-		# 	userwin = "OOOOO"
-		# 	if j == 0
-		# 		(0..diag).each do |i|				
-		# 			response += "#{self.boxes[i][diag-i]}"
-		# 		end
-		# 	elsif j == 1
-		# 		(0..9).each do |i|	
-		# 			response += "#{self.boxes[9-i][9+i-diag]}"
-		# 		end
-		# 	end
-		# 	if 	response.include?("XXXXX") 
-		# 		puts "Le joueur 1 à gagné par diag"
-		# 		@fin = true
-		# 	elsif response.include?("OOOOO")
-		# 		puts "Le joueur 2 à gagné par diag"
-		# 		@fin = true
-		# 	end
-		# end
-
-		# def is_game_over
-		# 	@fin
-		# end
-
-		def turn(i, j)
-			if @myturn
-				self.boxes[i][j].player = :user
-				@myturn = false
-				"C'est maintenant au tour du joueur 2 de rentrer des coordonnées"
-			else
-				self.boxes[i][j].player = :computer
-				#@myturn = true
-				"C'est maintenant au tour du joueur 1 de rentrer des coordonnées"
-			end
-		end
-
 	end
 
 	class Game
-		attr_accessor :board
+			attr_accessor :board
 
 		def initialize
 			self.board = Board.new
 		end
 
 		def turn(i, j)
-			if self.board.is_game_over 
+			if self.board.is_game_over
 				puts "Fin du jeu"
 			elsif self.board.boxes[i][j].player == :none
-				puts self.board.turn(i, j)
+				puts self.player(i, j)
 				show_board
 			else
+				#Rails.logger.info("test")
 				print "Cette case est déja prise"
 				puts
 				show_board
 			end
-
-			# (0..9).each do |i|
-			# 	self.board.checkrow(i)
-			# end
-			# (0..9).each do |i|
-			# 	self.board.checkcol(i)
-			# end
-
-			# (0..1).each do |j|
-			# 	(4..9).each do |i|
-			# 		self.board.checkdiag_sup_right(i, j)
-			# 	end
-			# end
-			
-			# (0..1).each do |j|
-			# 	(4..9).each do |i|
-			# 		self.board.checkdiag_sup_left(i, j)
-			# 	end
-			# end
 		end
 
-		# def tmp_fullfill
-		# 	(0..9).each do |i|
-		# 		(0..9).each do |j|
-		# 			self.board.boxes[i][j]="O"
-		# 		end
+		def player(i, j)
+			if @myturn
+				board.box(i,j).player = :user
+				@myturn = false
+				"C'est maintenant au tour du joueur 2 de rentrer des coordonnées"
+			else
+				board.box(i,j).player = :computer
+				@myturn = true
+				"C'est maintenant au tour du joueur 1 de rentrer des coordonnées"
+			end
+		end
 
-		# 	end
-		# 	show.board
-		# end
-
-		# def tmp_fivefill
-		# 	(0..5).each do |j|
-		# 		self.board.boxes[3][j]="O"
-		# 	end
-		# 	show_board
-		# end
+		def tmp_fivefill
+			(0..5).each do |j|
+				self.board.boxes[3][j]="O"
+			end
+			show_board
+		end
 
 		def show_board
 			puts self.board
 		end
-
 	end
+
 end
-
-
-
-#  g = Morpion::Game.new;g.board.alignments.count
-#  a = g.board.alignments[1];nil
-#  a.boxes.each do |box| box.player = :user end;nil
-#  g.show_board
-
