6925df34ab288c9ba4132824f2cb01b895136b42
Alexandre Morand <alexmorand26@gmail.com>
2015-10-23 18:43:50 +0200

    WIP

--- app/models/Morpion/game.rb
+++ app/models/Morpion/game.rb
@@ -31,9 +31,15 @@ module Morpion
     def initialize
       self.boxes = []
     end
+
     def is_won?
     	v = boxes.map(&:player).uniq
     	v.count == 1 && v.first != :none
+    end
+
+    def is_tie?
+    	t = boxes.map(&:player).uniq
+    	t.count < 2
     end
   end
 
@@ -87,6 +93,51 @@ module Morpion
     	end
 		end 
 
+    def find_best_boxes
+    	weight_user = [nil,2,5,10,20]
+    	weight_computer = [1,2,4,9,19]
+    	weight_boxes = []
+    	weight_box = 0
+    	weight_each_al = 0
+    	price = 0
+    	(0..9).each do |i|
+    		(0..9).each do |j|
+    			box(i,j).alignments.each do |al|
+    				(0..al.boxes.count).each do |each_al|
+	    				a = each_al.boxes.map(&:player)
+	    				if a.include?('user')
+	    					price += weight_user[a.include?('user').count]
+	    				else
+		    				price += weight_computer[a.include?('computer').count]
+		    			end
+	    			weight_each_al += price
+	    			end
+	    		end
+	    		weight_box += weight_each_al
+	    		weight_boxes <<  weight_box
+	    	end
+	    end
+	    puts weight_boxes
+	  end
+
+	# def all_alignments_per_box
+  #     (0 .. 9).each do |i|
+  #       (0 .. 9).each do |j|
+  #         box(i,j).alignments.each do |al|
+  #           al.boxes.each do |box|
+  #             box.player = :user
+  #           end
+  #         end
+  #         puts self
+  #         box(i,j).alignments.each do |al|
+  #           al.boxes.each do |box|
+  #             box.player = :none
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+
 		def box(i,j)
       boxes[i][j]
     end
@@ -106,6 +157,12 @@ module Morpion
 			result = alignments.select{|a| a.is_won?}
 			result.count > 0
 		end
+
+		def is_tie
+			result = alignments.select{|t| t.is_tie?}
+			result.count > 0
+		end
+
 	end
 
 	class Game
@@ -116,29 +173,48 @@ module Morpion
 		end
 
 		def turn(i, j)
-			if self.board.is_game_over
-				puts "Fin du jeu"
-			elsif self.board.boxes[i][j].player == :none
-				puts self.player(i, j)
-				show_board
-			else
-				#Rails.logger.info("test")
-				print "Cette case est déja prise"
-				puts
-				show_board
+			if i == "lourd" && j == "de_ouf"
+				puts "C'est gagné"
+			elsif self.board.is_game_over
+					puts "Vous avez fini cette partie \n 
+					Vous devez recommencer une partie"
+			elsif !self.board.is_tie
+					puts "Égalité, vous devez recommencer une partie"
+			elsif i<11 && j<11 && self.board.box(i,j).player == :none
+					puts self.player(i, j)
+			elsif i>10 || j>10
+					puts "Tu n'est pas dans le tableau \n 
+					Rentre un nombre inférieur à 10"
+			elsif i<11 && j<11 && self.board.box(i,j).player != :none
+				puts "Cette case est déja prise, essaie encore"
+			else 
+				puts "Que passa ???"
 			end
 		end
 
 		def player(i, j)
 			if @myturn
 				board.box(i,j).player = :user
-				@myturn = false
-				"C'est maintenant au tour du joueur 2 de rentrer des coordonnées"
+				if self.board.is_game_over
+					puts "C'est gagné"
+				elsif !self.board.is_tie
+					puts "Égalité"
+				else
+					@myturn = false
+					"C'est maintenant au joueur 2 de rentrer des coordonnées"
+				end
 			else
 				board.box(i,j).player = :computer
-				@myturn = true
-				"C'est maintenant au tour du joueur 1 de rentrer des coordonnées"
+				if self.board.is_game_over
+					puts "C'est gagné"
+				elsif !self.board.is_tie
+					puts "Égalité"
+				else
+					@myturn = true
+					"C'est maintenant au joueur 1 de rentrer des coordonnées"
+				end
 			end
+			show_board
 		end
 
 		def tmp_fivefill
